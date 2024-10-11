a1f99b1dfaff5f3efa5000f4bd861d6c7657a5c6
이형필 <hexagongame@naver.com>
2024-10-11 16:17:06 +0900

    초기 커밋.

---
+++ MyStudy/Dictionary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStudy
+{
+    public class Dictionary<TKey, TValue>
+    {
+        public List<KeyValuePair<TKey, TValue>> [] tables;
+
+
+
+    }
+
+    class Program
+    {
+        static void Main( string [] args )
+        {
+            List<int> list = new List<int>();
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(5);
+
+            Console.WriteLine(list.Get(0));
+        }
+    }
+}
---
+++ MyStudy/List.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MyStudy
+{
+    public class List<T>
+    {
+        T [] values;
+        int count;
+        int capacity;
+
+
+        public List( int defaultCapacity = 4 )
+        {
+            capacity = defaultCapacity;
+            values = new T [capacity];
+            count = 0;
+        }
+
+        public T Add( T value )
+        {
+            if ( count > capacity )
+            {
+                Resize();
+            }
+            return values [count++] = value;
+
+        }
+        public T Get( int index )
+        {
+            if ( index >= count || index < 0 )
+            {
+                throw new IndexOutOfRangeException("OutRange");
+            }
+            return values [index];
+        }
+        void Resize()
+        {
+            capacity *= 2;
+            T [] newValues = new T [capacity];
+            Array.Copy(values, newValues, count);
+            values = newValues;
+        }
+    }
+}
+
+
+
