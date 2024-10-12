65a325ec260fb5854f6b8904a355acbdf5872097
이형필 <hexagongame@naver.com>
2024-10-12 18:41:50 +0900

    feat : CodeTest 7 ~ 13

--- MyStudy/CodeTest/Class1.cs
+++ MyStudy/CodeTest/Class1.cs
@@ -10,17 +10,10 @@ namespace MyStudy.CodeTest
     {
         public static void Main( string [] args )
         {
-            string s = "str";
-
-            Console.Clear();
-            Console.WriteLine(s);
+            
         }
-    }
 
-
-    public class Class2
-    {
-        public static void Main( string [] args )
+        public void Test2( string [] args )
         {
             Console.Clear();
             string [] s = Console.ReadLine().Split(' ');
@@ -31,11 +24,8 @@ namespace MyStudy.CodeTest
             Console.WriteLine($"a = {a}");
             Console.WriteLine($"b = {b}");
         }
-    }
 
-    public class Class3
-    {
-        public static void Main( string [] args )
+        public void Test( string [] args )
         {
             Console.Clear();
             string [] stringArray = Console.ReadLine().Split(' ');
@@ -46,11 +36,8 @@ namespace MyStudy.CodeTest
                 Console.Write(s);
             }
         }
-    }
 
-    public class Class4
-    {
-        public static void Main( string [] args )
+        public void Test4( string [] args )
         {
             string str = Console.ReadLine();
 
@@ -59,33 +46,26 @@ namespace MyStudy.CodeTest
 
 
             Console.WriteLine("!@#$%^&*(\\'\"<>?:;");               // 특수문자
-        }
 
-        static string ChangeCase( string str )
-        {
-            char [] chars = str.ToCharArray();
-
-            for ( int i = 0; i < chars.Length; i++ )
+            static string ChangeCase( string str )
             {
-                if ( char.IsUpper(chars [i]) )
+                char [] chars = str.ToCharArray();
+
+                for ( int i = 0; i < chars.Length; i++ )
                 {
-                    chars [i] = char.ToLower(chars [i]);
+                    if ( char.IsUpper(chars [i]) )
+                    {
+                        chars [i] = char.ToLower(chars [i]);
+                    }
+                    else if ( char.IsLower(chars [i]) )
+                    {
+                        chars [i] = char.ToUpper(chars [i]);
+                    }
                 }
-                else if ( char.IsLower(chars [i]) )
-                {
-                    chars [i] = char.ToUpper(chars [i]);
-                }
+                return new string(chars);
             }
-            return new string(chars);
         }
-
-
-
-    }
-
-    public class Class5
-    {
-        public static void Main(string [] args)
+        public void Test5( string [] args )
         {
             string [] s;
 
@@ -97,11 +77,8 @@ namespace MyStudy.CodeTest
             int c = a + b;
             Console.WriteLine($"{a} + {b} = {c}");
         }
-    }
 
-    public class Class6
-    {
-        public static void Main( string [] args)
+        public void Test6( string [] args )
         {
             string s;
 
@@ -114,10 +91,8 @@ namespace MyStudy.CodeTest
                 Console.WriteLine(chars [i]);
             }
         }
-    }
-    public class Class7
-    {
-        public void Class()
+
+        public void Test7()
         {
             String s;
 
@@ -126,7 +101,7 @@ namespace MyStudy.CodeTest
 
             int a = Int32.Parse(s);
 
-            if(a%2 == 0 )
+            if ( a % 2 == 0 )
             {
                 Console.WriteLine($"{a} is even");
             }
@@ -135,11 +110,86 @@ namespace MyStudy.CodeTest
                 Console.WriteLine($"{a} is odd");
             }
         }
+        public string Test8( string my_string, string overwrite_string, int s )
+        {
+            char [] chars = my_string.ToCharArray();
 
+
+            for ( int i = 0; i < overwrite_string.Length; i++ )
+            {
+                chars [s + i] = overwrite_string [i];
+            }
+
+            return new string(chars);
+        }
+
+        public string Test9(string str1,string str2 )
+        {
+            char [] chars = str1.ToCharArray();
+            char [] chars2 = str2.ToCharArray();
+
+            char [] resultChar = new char [chars.Length + chars2.Length];
+
+            for ( int i = 0; i < chars.Length; i++)
+            {
+                resultChar [2 * i] = chars [i];
+            }
+            for(int i = 0; i < chars2.Length; i++ )
+            {
+                resultChar [2 * i + 1] = chars2 [i];
+            }
+
+            return new string(resultChar);
+        }
+
+        public string Test10( string [] arr )
+        {
+            string answer = String.Join("", arr);
+
+            return answer;
+        }
+        public string Test11( string my_string, int k )
+        {
+            string answer = "";
+            for(int i = 0; i < k; i++ )
+            {
+                answer += my_string; // my_string을 answer에 이어 붙임
+            }
+
+             answer = String.Concat(Enumerable.Repeat(my_string,k));
+            return answer;
+        }
+        public int Test12( int a, int b )
+        {
+            // a와 b를 문자열로 변환하여 결합
+            string ab = $"{a}{b}"; // a ⊕ b
+            string ba = $"{b}{a}"; // b ⊕ a
+
+            // 두 문자열을 정수로 변환하여 비교
+            int resultAb = int.Parse(ab);
+            int resultBa = int.Parse(ba);
+
+            // 더 큰 값을 반환, 같으면 ab를 반환
+            return resultAb >= resultBa ? resultAb : resultBa;
+        }
+        public int Test13( int a, int b )
+        {
+            int answer = 0;
+            string ab = $"{a}{b}";
+            int abValue = int.Parse(ab);
+            int cross = 2 * a * b;
+
+            answer = abValue >= cross ? abValue : cross;
+            if ( abValue == cross )
+                answer = abValue;
+
+            return answer;
+        }
     }
 
 
-    source :  https://school.programmers.co.kr/learn/challenges
+
+//    source :  https://school.programmers.co.kr/learn/challenges
 }
 
 
